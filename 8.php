7ca6f64d8d152394f13c04edac30993f18ffeb61
[dinithi] <[dinithimweerasinghe95@gmail.com>
2019-03-12 23:38:44 +0530

    php 8
    php 8

---
+++ 8.php
@@ -0,0 +1,16 @@
+<html>
+<title>php</title>
+<body>
+<?php
+$x = 5;
+$y = 10;
+
+function myTest() {
+    $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
+} 
+
+myTest();
+echo $y; 
+?>
+</body>
+</html>
